1c54acfc0874000e68eefb2f5a945249ccae4f8b
M4y4Ti4 <mayakanani@hotmail.com>
2026-03-04 11:20:10 +0100

    resample and plot uncorrected IR

--- examples/scenario1/clean_results.py
+++ examples/scenario1/clean_results.py
@@ -1,8 +1,9 @@
-import numpy
+import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
 import os
 
+""" to plot the result from main_HF.py
 # Load the result
 script_dir = os.path.dirname(os.path.abspath(__file__))
 result_path = os.path.join(script_dir, "result.mat")
@@ -30,3 +31,66 @@ for i in range(prec.shape[0]):
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+    """
+
+
+"""to plot the result from main.py"""
+script_dir = os.path.dirname(os.path.abspath(__file__))
+result_path = os.path.join(script_dir, "result_plswork.mat")
+
+print(f"Loading from: {result_path}")
+data = scipy.io.loadmat(result_path)
+
+IR = data["IR"] #impulse response data
+TR = data["TR"] #transfer function
+freqs = data["freqs"].squeeze() #freq vector
+dt_new = float(data["dt_new"].item()) #upsampled dt
+sampling_freq = float(data["sampling_freq"].item()) #should be 44100??
+IR_uncorrected = data["IR_Uncorrected"] #raw prec before correction
+dt_simulation = float(data["dt_simulation"].item()) #original simulation dt
+
+print(f"Sampling frequency: {sampling_freq} Hz")
+print(f"Simulation dt: {dt_simulation:.6f} s  =>  {1/dt_simulation:.1f} Hz")
+print(f"IR shape: {IR.shape}")
+print(f"Number of receivers: {IR.shape[0]}")
+print(f"uncorrected IR shape: {IR_uncorrected.shape}")
+print(f"corrected IR shape: {IR.shape}")
+
+"""
+#plot corrected IR
+t_new = np.arange(IR.shape[1]) * dt_new
+plt.plot(t_new, IR[0])
+plt.xlabel("time (s)")
+plt.ylabel("amplitude")
+plt.title("corrected RIR (after source spectrum correction)")
+plt.show()
+"""
+
+#plot uncorrected (raw) IR (FROM MAIN.PY)
+t_old = np.arange(IR_uncorrected.shape[1]) * dt_simulation
+plt.plot(t_old, IR_uncorrected[0])
+plt.xlabel("time (s)")
+plt.ylabel("uncorrected amplitude")
+plt.title("uncorrected RIR")
+plt.show()
+
+#resample uncorrected IR (FROM MAIN.PY)
+import scipy.signal
+
+fs_dg = 1/dt_simulation
+fs_target = 44100 
+
+n_samples_new = int(IR_uncorrected[0].shape[0] * fs_target / fs_dg)
+IR_dg_resampled = scipy.signal.resample(IR_uncorrected[0], n_samples_new)
+
+print(f"DG IR resampled from {fs_dg: .1f} Hz to {fs_target} Hz")
+print(f"Original samples: {IR_uncorrected.shape[1]}, resampled: {n_samples_new}")
+
+dt_resampled = 1 / fs_target
+t_resampled = np.arange(len(IR_dg_resampled)) * dt_resampled 
+
+plt.plot(t_resampled, IR_dg_resampled)
+plt.title("Resampled uncorrected IR")
+plt.xlabel("time (s)")
+plt.ylabel("pressure")
+plt.show()
